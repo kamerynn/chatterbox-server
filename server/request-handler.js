22bc18f5f0bb2c87d56ade41b0c7c84f86dd667f
Kamerynn Harrah <kamharrah8@gmail.com>
2015-07-21 19:35:56 -0400

    Refactor basic-server to act as the router

--- server/request-handler.js
+++ server/request-handler.js
@@ -11,16 +11,13 @@ var messages = [
   }
 ]
 
-exports.requestHandler = function(request, response) {
-  console.log("Serving request type " + request.method + " for url " + request.url);
-
+exports.requestHandler = function(request, response, pathname) {
   var action = actions[request.method];
-  action ? action(request, response) : helpers.sendResponse(response, undefined, 404);
+  action ? action(request, response, pathname) : helpers.sendResponse(response, undefined, 404);
 };
 
 var actions = {
-  GET: function(request, response) {
-    var pathname = url.parse(request.url).pathname
+  GET: function(request, response, pathname) {
     if (pathname === '/classes/messages') {
       helpers.sendResponse(response, {results: messages});
     }
@@ -28,8 +25,7 @@ var actions = {
     if (pathname === '/classes/room') {}
   },
 
-  POST: function(request, response) {
-    var pathname = url.parse(request.url).pathname
+  POST: function(request, response, pathname) {
     if (pathname === '/send') {
       helpers.collectData(request, function(message) {
         messages.push(message);
