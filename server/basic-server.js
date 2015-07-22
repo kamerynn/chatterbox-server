0dd9d73bebf30060b4d55ab31cab16244b69cf07
Kamerynn Harrah <kamharrah8@gmail.com>
2015-07-21 20:37:12 -0400

    Add express framework and refactor basic-server GET to utilize express

--- server/basic-server.js
+++ server/basic-server.js
@@ -2,24 +2,37 @@ var http = require("http");
 var url = require("url");
 var rh = require("./request-handler.js");
 var helpers = require("./http-helpers.js");
+var express = require("express");
+var app = express();
 
-var port = 3000;
-var ip = "127.0.0.1";
+var server = app.listen(3000, function() {
+  var host = "127.0.0.1";
+  var port = server.address().port;
+  console.log("Listening at http://%s:%s", host, port);
+})
 
-var routes = {
-  '/classes/messages': rh.requestHandler,
-  '/send' : rh.requestHandler
-}
-
-var server = http.createServer(function(request, response) {
-  console.log("Serving request type " + request.method + " for url " + request.url);
-  var pathname = url.parse(request.url).pathname;
-  var route = routes[pathname];
-  if (route) {
-    route(request, response, pathname);
-  } else {
-    helpers.sendResponse(response, undefined, 404);
+var objectId = 1;
+var messages = [
+  {
+    username: 'Kam',
+    text: 'yooo',
+    objectId: objectId
   }
-});
-console.log("Listening on http://" + ip + ":" + port);
-server.listen(port, ip);
+]
+
+// Set headers
+app.use(function(req, res, next) {
+  res.header("access-control-allow-origin", "*");
+  res.header("access-control-allow-methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("access-control-allow-headers", "content-type, accept");
+  res.header("access-control-max-age", 10);
+  next();
+})
+
+app.get('/', function(req, res) {
+  res.send("hello world!");
+})
+
+app.get('/classes/messages', function(req, res) {
+  res.json({results: messages});
+})
